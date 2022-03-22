0030fe4d3a0bc63fbf9ee4bb088eeeb693ffb6e8
h-miiaah <haim.miiaah5@gmail.com>
2022-03-21 23:14:58 -0400

    comments

--- PowerOfTwo.js
+++ PowerOfTwo.js
@@ -17,5 +17,5 @@
  * If n is 0, then it’s not a power of 2.
  * If n is 1, then it’s a power of 2.
  * If n is not a power of 2, then it’s not a power of 2.
- * 
+ * If n is a power of 2, then keep dividing it by 2 until it’s no longer a power of 2.
  */
